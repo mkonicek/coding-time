f77c61a848d4fa223e5417f0d8445dbfcebd6c4a
Martin Konicek <martin.konicek@gmail.com>
2020-12-28 15:32:09 +0000

    Copyright in footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © Martin Konicek {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
