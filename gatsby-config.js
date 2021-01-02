047113fda6679790d72f3709d5ae64106efd854b
Martin Konicek <martin.konicek@gmail.com>
2021-01-02 19:28:24 +0000

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-Z37TYKLNGK`,
       },
     },
     `gatsby-plugin-feed`,
